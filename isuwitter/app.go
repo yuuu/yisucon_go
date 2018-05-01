e4ab88a864afbb3d58ad46cbda03ba98f7bd8233
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-05-01 22:41:57 +0900

    検索をSQLで

--- isuwitter/app.go
+++ isuwitter/app.go
@@ -472,7 +472,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	var rows *sql.Rows
 	var err error
 	if until == "" {
-		rows, err = db.Query(`SELECT * FROM tweets ORDER BY id DESC`)
+		rows, err = db.Query(`SELECT * FROM tweets WHERE text LIKE ? ORDER BY id DESC`, "%"+query+"%")
 	} else {
 		rows, err = db.Query(`SELECT * FROM tweets WHERE created_at < ? ORDER BY created_at DESC`, until)
 	}
